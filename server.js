091fa9e3cdf2b65217f6cbce4bbddb6c2ff43158
Victor Velasquez <vvelasquez@safefleet.net>
2021-12-03 19:08:46 -0500

    add new player

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express")
 const path = require('path')
-const app = express();
+const app = express()
 const port = 5000
 const mongoose = require("mongoose")
 const { graphqlHTTP } = require("express-graphql")
@@ -13,31 +13,36 @@ var cors = require('cors')
 const extensions = ({ context }) => {
   return {
     runTime: Date.now() - context.startTime,
-  };
-};
+  }
+}
 
-app.use(logger);
+app.use(logger)
 app.use(cors({
   //origin: ['https://www.section.io', 'https://www.google.com/']
   origin: 'http://localhost:3000'
 }))
+
+if(process.env.DEBUG) {
+  console.log(`Starting in debug mode...`)
+  mongoose.set('debug', process.env.DEBUG)
+}
 
 app.listen(port, async () => {
   console.log(`Server is listening at http://localhost:${port}`)
   await mongoose.connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  });
-});
+  })
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 mongoose.connection.on(
   "error",
   console.error.bind(console, "MongoDB connection error:")
-);
+)
 
-const graphqlSchema = require("./schemas/index");
+const graphqlSchema = require("./schemas/index")
 
 app.use(
   "/graphql",
@@ -47,14 +52,14 @@ app.use(
       graphiql: true,
       schema: graphqlSchema,
       extensions,
-    };
+    }
   })
-);
+)
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname , '/public/index.html'));
-});
+    res.sendFile(path.join(__dirname , '/public/index.html'))
+})
 
 // // Recurrent Jobs Configuration
 // const scheduler = new ToadScheduler()
@@ -70,6 +75,6 @@ app.get('*', (req,res) =>{
 
 // // Run once at startup
 // const start = async function () {
-//   await statsJob();
+//   await statsJob()
 // }
-// // start();
+// // start()
